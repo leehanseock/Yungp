ce736a4b6a0be95d50b99672a095992b7845f64c
leehanseock <leehanseock@gmail.com>
2025-05-08 12:12:17 +0900

    Initial commit

---
+++ myproject.cmd
@@ -0,0 +1,5 @@
+@echo off
+CALL C:\Users\AISW-509-184\anaconda3\Scripts\activate.bat venv
+set FLASK_APP=pybo
+set FLASK_DEBUG=true
+cd /d D:\Yungp
---
+++ pybo/__init__.py
@@ -0,0 +1,21 @@
+from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+import config
+db = SQLAlchemy()
+migrate = Migrate()
+
+def create_app():
+    app = Flask(__name__)  # 플라스크 애플리케이션을 생성
+    app.config.from_object(config)
+
+    # ORM
+    db.init_app(app)
+    migrate.init_app(app, db)
+
+    # Blueprint
+    from .views import main_views
+    app.register_blueprint(main_views.bp)
+
+    return app
